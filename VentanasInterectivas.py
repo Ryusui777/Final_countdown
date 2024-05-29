70b49d8fa63efeaf1f7fe499c682062457f188ea
Bran <brandonalfarosaborio@gmail.com>
2024-05-29 14:37:00 -0600

    avance

--- VentanasInterectivas.py
+++ VentanasInterectivas.py
@@ -3,3 +3,21 @@ import sys
 from Botones import Button
 
 pygame.init()
+
+WIDHT, HEIGHT= 600,600
+window= pygame.display.set_mode((WIDHT, HEIGHT))
+pygame.display.set_caption("Mario 3")
+fondo=pygame.transform.scale(pygame.image.load("BG.jpg"), (WIDHT, HEIGHT))
+
+def letra(size):
+    return pygame.font.Font("assents/font.ttf", size)
+
+def menu_principal():
+    pygame.display.set_caption("Menu principal")
+
+    while True:
+        window.blit(fondo,(0,0))
+
+        posicion_del_mouse_menu= pygame.mouse.get_pos()
+        menu_texto= letra(100).render("Menu Principal", True, "#b68f40")
+        menu_rect= menu_texto.get_rect(centro= (640, 100))
