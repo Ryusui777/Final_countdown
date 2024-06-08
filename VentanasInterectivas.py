1a2ee3fec5f06fe8178899b15d8dd19c8957a83f
Bran <brandonalfarosaborio@gmail.com>
2024-06-08 08:43:41 -0600

    color de relas

--- VentanasInterectivas.py
+++ VentanasInterectivas.py
@@ -85,7 +85,7 @@ def menu_principal():
         boton_jugar= Button (imagen=pygame.image.load("Rect.png"), posicion=(640, 250), 
                             texto_entrada= "Jugar", fuente= letra(75), color_base= "White", hovering_color="Green")
         boton_reglas= Button (imagen=pygame.image.load("Rect.png"), posicion=(640, 400), 
-                            texto_entrada= "Reglas", fuente= letra(75), color_base= "White", hovering_color="Green")
+                            texto_entrada= "Reglas", fuente= letra(75), color_base= "White", hovering_color="Purple")
         boton_salir= Button (imagen=pygame.image.load("Rect.png"), posicion=(640, 550), 
                             texto_entrada= "Salir", fuente= letra(75), color_base= "White", hovering_color="Red")
     
