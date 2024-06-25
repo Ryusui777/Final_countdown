58163cd8e4d2d7333fa4902530b8bac4ba22449d
Bran <brandonalfarosaborio@gmail.com>
2024-06-25 16:45:56 -0600

    changes

--- Menu Principal.py
+++ Menu Principal.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from Botones import Button
 from screeninfo import get_monitors
-from char_select_win import SelectWindow  # Importar SelectWindow
+from char_select_win import SelectWindow  
 import Player
 
 def get_resolution() -> tuple:
@@ -32,7 +32,6 @@ class Game:
         self.state = "Menu_principal"
         self.states = {
             "Menu_principal": self.menu_principal,
-            "Start": self.videojuego,
             "Reglas": self.reglas,
             "Char_Select": self.char_select_win  # Nuevo estado
         }
@@ -65,10 +64,8 @@ class Game:
                 if evento.type == pygame.MOUSEBUTTONDOWN:
                     if boton_jugar.verificar_input(mouse_pos):
                         self.set_state("Char_Select")  # Cambiar al nuevo estado
-                        return
                     if boton_reglas.verificar_input(mouse_pos):
                         self.set_state("Reglas")
-                        return
                     if boton_salir.verificar_input(mouse_pos):
                         pygame.quit()
                         sys.exit()
@@ -77,42 +74,55 @@ class Game:
     def char_select_win(self):
         win = SelectWindow()
         players = [Player.Player(), Player.Player(), Player.Player()]
+        loaded_players = [player.selection_character_give_frame() for player in players]
         run = True
         clock = pygame.time.Clock()
+        win.refresh(loaded_players)
+        
+        mas_imgen = pygame.image.load("mas.png")
+        mas_imgen = pygame.transform.scale(mas_imgen, (100, 100)) 
+        boton_mas = Button(imagen=mas_imgen, posicion=(resolution[0] -135, resolution[1] - 400),
+                                        texto_entrada=None, fuente=letra(75), color_base="White", hovering_color="white")
+        
+
+        menos_imgen = pygame.image.load("menos.png")
+        menos_imgen = pygame.transform.scale(menos_imgen, (100, 100)) 
+        boton_menos = Button(imagen=menos_imgen, posicion=(resolution[0] -135, resolution[1]  - 270),
+                                        texto_entrada=None, fuente=letra(75), color_base="White", hovering_color="white")
+        
+        derecha_imgen = pygame.image.load("Flecha para la derecha.png")
+        derecha_imgen = pygame.transform.scale(derecha_imgen, (80, 80)) 
+        boton_derecha = Button(imagen=derecha_imgen, posicion=(resolution[0]/2+170, resolution[1]/2 -350),
+                                        texto_entrada=None, fuente=letra(75), color_base="White", hovering_color="white")
+        
+
+        izquierda_imgen = pygame.image.load("Flecha para la izquierda.png")
+        izquierda_imgen = pygame.transform.scale(izquierda_imgen, (80, 80)) 
+        boton_izquierda = Button(imagen=izquierda_imgen, posicion=(resolution[0] / 2 + -170, resolution[1] / 2 - 350),
+                                        texto_entrada=None, fuente=letra(75), color_base="White", hovering_color="white")
+        
+        boton_volver = Button(imagen=None, posicion=(resolution[0] / 2 - 720, resolution[1] / 2 + 400),
+                               texto_entrada="Volver", fuente=letra(50), color_base="white", hovering_color="Yellow")
         
         while self.state == "Char_Select":
             clock.tick(24)
             loaded_players = [player.selection_character_give_frame() for player in players]
             win.refresh(loaded_players)
             
-            for evento in pygame.event.get():
-                if evento.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if evento.type == pygame.MOUSEBUTTONDOWN:
-                    # Aquí puedes manejar las selecciones de personajes y transiciones posteriores
-                    pass
-
-            pygame.display.update()
-
-    def videojuego(self):
-        BG = pygame.transform.scale(pygame.image.load("BranyDavid.jpg"), resolution)
-        videojuego_volver = Button(imagen=None, posicion=(resolution[0] / 2 - 720, resolution[1] / 2 + 400),
-                              texto_entrada="Volver", fuente=letra(50), color_base="White", hovering_color="Yellow")
-
-        while self.state == "Start":
-            self.screen.blit(BG, (0, 0))
-            videojuego_volver.cambiar_color(pygame.mouse.get_pos())
-            videojuego_volver.actualizar(self.screen)
+            mouse_pos = pygame.mouse.get_pos()
+            
+            for button in [boton_mas,boton_menos,boton_derecha,boton_izquierda, boton_volver]:
+                button.cambiar_color(mouse_pos)
+                button.actualizar(win.window_object)
             
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if evento.type == pygame.MOUSEBUTTONDOWN:
-                    if videojuego_volver.verificar_input(pygame.mouse.get_pos()):
+                    if boton_volver.verificar_input(mouse_pos):
                         self.set_state("Menu_principal")
-                        return
+            
             pygame.display.update()
 
     def reglas(self):
@@ -138,7 +148,6 @@ class Game:
                 if evento.type == pygame.MOUSEBUTTONDOWN:
                     if reglas_volver.verificar_input(mouse_pos):
                         self.set_state("Menu_principal")
-                        return
             pygame.display.update()
 
     def run(self):
